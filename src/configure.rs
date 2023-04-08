0b577cf3b7d3724bb51ee85936a88fdfa05250fb
Alexander Koz <a@koz.email>
2023-04-08 13:27:58 +0400

    a little CLI cfg changes

--- src/configure.rs
+++ src/configure.rs
@@ -9,13 +9,14 @@ use crate::format::DeckType;
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Name of the file with deck list
-    #[arg(long)]
+    #[arg(short, long)]
     pub deck: PathBuf,
     /// number of turn
     #[arg(short, long)]
     pub turn: u8,
     ///Your deck type
-    #[arg(short, long)]
+	 // TODO: Maybe we should make it optional or with default value?
+    #[arg(long="type")]
     pub deck_type: DeckType,
 }
 
